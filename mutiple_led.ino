d7145bfa731a7004ef3ed1f16eb7ef7d9f35a281
Reviving India <47768315+RevivingIndia@users.noreply.github.com>
2020-07-12 16:27:11 +0530

    Add files via upload

---
+++ mutiple_led.ino
@@ -0,0 +1,23 @@
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(7, HIGH);// turn the LED on (HIGH is the voltage level)
+  delay(1000);      // wait for a second
+  digitalWrite(8, HIGH);
+  delay(1000);
+  digitalWrite(9, HIGH);// turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(7, LOW); 
+  delay(1000);
+  digitalWrite(8, LOW);
+  delay(1000);
+  digitalWrite(9, LOW);// turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
